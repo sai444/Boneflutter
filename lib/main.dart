72fb101d2d9559b6508f79ec1b00eb664b49487f
sai444 <31222470+sai444@users.noreply.github.com>
2021-07-09 00:02:44 +0530

    09_07

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+          child: Center(
+        child: Text('welcome app'),
+      )),
+    );
+  }
+}
