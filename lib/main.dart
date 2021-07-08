3b491c91f7c9817641dc1950bda96428b7c9f8a3
sai444 <31222470+sai444@users.noreply.github.com>
2021-07-09 01:17:57 +0530

    day3 complated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:bone01/home_page.dart';
+import 'package:bone01/pages/home_page.dart';
+import 'package:bone01/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,6 +9,17 @@ void main() {
 class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.green),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
+    );
   }
 }
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+        child: Text(
+          'LoginPage',
+          style: TextStyle(
+              fontSize: 40, color: Colors.blue, fontWeight: FontWeight.bold),
+          textScaleFactor: 1.2,
+        ),
+      ),
+    );
+  }
+}
